9372303246727acbd1d68205e57331b8be053449
GuadalupeGFigueroa <guadalupe.g.figueroa@gmail.com>
2024-12-08 03:05:44 +0100

    Initial configuration done. UML and ER diagrams added

---
+++ src/test/java/dev/guadalupe/bancodeltiempo/BancodeltiempoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.guadalupe.bancodeltiempo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancodeltiempoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
