c5b0c0c9a63b38ec39e6b2525d9e6db9ae5dbe0e
GuadalupeGFigueroa <guadalupe.g.figueroa@gmail.com>
2024-12-09 21:15:56 +0100

    UserRepository file created

--- src/main/java/dev/guadalupe/bancodeltiempo/user/UserRepository.java
+++ src/main/java/dev/guadalupe/bancodeltiempo/user/UserRepository.java
@@ -1,5 +1,13 @@
 package dev.guadalupe.bancodeltiempo.user;
 
-public class UserRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // Encuentra un usuario por email
+    Optional<User> findByEmail(String email);
+
+    // Encuentra un usuario por username (si existe en tu modelo)
+    Optional<User> findByUsername(String username);
 }
