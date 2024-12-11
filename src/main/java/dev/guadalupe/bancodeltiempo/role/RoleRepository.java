e00121dfbc25edd3d733041a905cc83bcf6ab8a0
GuadalupeGFigueroa <guadalupe.g.figueroa@gmail.com>
2024-12-11 21:39:13 +0100

    deleted roles and account folders

--- src/main/java/dev/guadalupe/bancodeltiempo/role/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.guadalupe.bancodeltiempo.role;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    // Buscar un rol por nombre
-    Optional<Role> findByName(String name);
-}
