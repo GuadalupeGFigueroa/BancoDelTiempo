bbbea074d389af0d11f0a69e1845f934f051f9d6
GuadalupeGFigueroa <guadalupe.g.figueroa@gmail.com>
2024-12-11 02:08:06 +0100

    DTO User created

--- src/main/java/dev/guadalupe/bancodeltiempo/user/User.Dto.java
+++
@@ -1,5 +0,0 @@
-package dev.guadalupe.bancodeltiempo.user;
-
-public class User.Dto {
-    
-}
---
+++ src/main/java/dev/guadalupe/bancodeltiempo/user/UserDto.java
@@ -0,0 +1,74 @@
+package dev.guadalupe.bancodeltiempo.user;
+
+import dev.guadalupe.bancodeltiempo.user.User;
+
+public class UserDto {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private Role role;
+
+    public UserDto(User user) {
+        this.id = user.getId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.phoneNumber = user.getPhoneNumber();
+        this.role = user.getRole();
+    }
+
+    public UserDto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }  
+    
+    
+}
